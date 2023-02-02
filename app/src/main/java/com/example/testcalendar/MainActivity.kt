95d5ecc2ce68126b243bfebefc24df861ea365b2
rustam <rustamalimidjanov@gmail.com>
2023-02-02 21:14:07 +0300

    add custom calendar api

--- app/src/main/java/com/example/testcalendar/MainActivity.kt
+++ app/src/main/java/com/example/testcalendar/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.testcalendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.applandeo.materialcalendarview.EventDay
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener
+import com.applandeo.materialcalendarview.listeners.OnSelectDateListener
+import com.example.testcalendar.databinding.ActivityMainBinding
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnDayClickListener, OnSelectDateListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calendarView.setOnDayClickListener(this)
+    }
+
+    override fun onDayClick(eventDay: EventDay) {
+        val event = eventDay.calendar.time.toString()
+        val toast: Toast = Toast.makeText(applicationContext, event, Toast.LENGTH_SHORT)
+        toast.show()
+
+    }
+
+    override fun onSelect(calendar: List<Calendar>) {
+        TODO("Not yet implemented")
     }
 }
